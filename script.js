3c5a586440f4025458ad6cd18383c81c30b41831
edword03 <veretennikov_dmitriy01@mail.ru>
2020-08-07 13:43:10 +0400

    feat: was added a few new features

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
-
-alert('succefull');
-console.log('true');
+let money = 150000;
+let income = 'freelance';
+let addExpenses = 'Еда, Жилье, ТелеФон, интЕрнет';
+let deposit = true;
+let mission = 1000000;
+let period = 12,
+    budgetDay = money / 30;
+    
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log(`Период равен ${period} месяцев.`, `Цель заработать ${mission} долларов`);
+console.log(addExpenses.toLowerCase().split());
+console.log('budgetDay: ', budgetDay);
