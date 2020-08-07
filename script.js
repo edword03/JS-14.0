593bfb5124bc33b83c9d2db26f3539fff16a026c
edword03 <veretennikov_dmitriy01@mail.ru>
2020-08-07 21:40:16 +0400

    fix: fixed addExpenses

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let money = 150000;
-let income = 'freelance';
+const money = 150000,
+      income = 'freelance';
 let addExpenses = 'Еда, Жилье, ТелеФон, интЕрнет';
 let deposit = true;
-let mission = 1500000;
-let period = 12,
-    budgetDay = money / 30;
+const mission = 1500000,
+      period = 12,
+      budgetDay = money / 30;
     
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
 console.log(addExpenses.length);
 console.log(`Период равен ${period} месяцев.`, `Цель заработать ${mission} долларов`);
-console.log(addExpenses.toLowerCase().split());
+console.log(addExpenses.toLowerCase().split(','));
 console.log('budgetDay: ', budgetDay);
